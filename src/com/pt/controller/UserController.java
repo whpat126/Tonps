fc75b9cbaaf1b84b8547822bb7e78716c26fa76a
songqi <1668727676@qq.com>
2015-07-29 15:14:31 +0800

    第一版用户中心的controller完成

--- src/com/pt/controller/UserController.java
+++ src/com/pt/controller/UserController.java
@@ -2,6 +2,8 @@ package com.pt.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.gson.Gson;
+import com.pt.domain.Pwdq;
 import com.pt.domain.User;
 import com.pt.service.UserService;
 import com.qq.connect.QQConnectException;
@@ -132,8 +136,8 @@ public class UserController {
 	}
 
 	/**
-	 * 用户在访问页面时，会使用自动登录功能,如果获得的session为null，则返回false
-	 * author：songqi
+	 * 用户在访问页面时，会使用自动登录功能,如果获得的session为null，则返回false author：songqi
+	 * 
 	 * @param session
 	 * @param request
 	 * @param resp
@@ -154,24 +158,220 @@ public class UserController {
 	}
 
 	/**
-	 * 用户使用qq登录系统
-	 * author：songqi
+	 * 用户使用qq登录系统 author：songqi
+	 * 
 	 * @param request
 	 * @param response
 	 * @param session
 	 * @throws IOException
 	 */
-	@RequestMapping(value = "/qqLogin", method = {RequestMethod.GET,RequestMethod.POST})
+	@RequestMapping(value = "/qqLogin", method = { RequestMethod.GET,
+			RequestMethod.POST })
 	private void qqLogin(HttpServletRequest request,
 			HttpServletResponse response, HttpSession session)
 			throws IOException {
 		response.setContentType("text/html;charset=utf-8");
-        try {
-            response.sendRedirect(new Oauth().getAuthorizeURL(request));
-            System.out.println("进入qqLogin方法");
-        } catch (QQConnectException e) {
-            e.printStackTrace();
-        }
+		try {
+			response.sendRedirect(new Oauth().getAuthorizeURL(request));
+			System.out.println("进入qqLogin方法");
+		} catch (QQConnectException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 用户点击修改密码后，页面通过controller跳转到web-inf下的jsp中 author：songqi
+	 * 
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping("/modifyPwd")
+	private void modifyPwd(HttpServletResponse response, HttpSession session,
+			String opwd, String pwd, String newpwd) throws IOException {
+		PrintWriter out = response.getWriter();
+		String username = (String) session.getAttribute("userName");
+		// boolean flag = us. //调用service的验证方法参数：username,opwd
+		boolean flag = false;
+		flag = true;
+		if (!flag) { // 密码不正确 flag=false
+			out.print(false);
+		} else {
+			// flag = us.u // 传入新密码
+			if (flag) {
+				out.print("success");
+			} else {
+				out.print("fail");
+			}
+		}
+
+	}
+
+	/**
+	 * 验证邮箱，给指定邮箱发送邮件 author：songqi
+	 * 
+	 * @param response
+	 * @param session
+	 * @param opwd
+	 * @throws IOException
+	 */
+	@RequestMapping("/validateEmail")
+	private void validateEmail(HttpServletResponse response,
+			HttpSession session, String opwd) throws IOException {
+		PrintWriter out = response.getWriter();
+		String username = (String) session.getAttribute("userName");
+		// boolean flag = // 调用发送邮件的方法
+		boolean flag = false;
+		flag = true;
+		if (flag)
+			out.print(true);
+		else
+			out.print(false);
+	}
+
+	/**
+	 * 查询邮箱是否已经验证，如果已经验证则返回到jsp，如果没有验证则让用户输入进行验证 author：songqi
+	 * 
+	 * @param response
+	 * @param session
+	 * @param opwd
+	 * @throws IOException
+	 */
+	@RequestMapping("queryEmail")
+	private void queryEmail(HttpServletResponse response, HttpSession session,
+			String opwd) throws IOException {
+		PrintWriter out = response.getWriter();
+		String username = (String) session.getAttribute("userName");
+		// String str = // 获取邮箱的方法
+		String str = "abc.com";
+		if ("".equals(str) || null == str)
+			out.print(false);
+		else
+			out.print(str);
+	}
+
+	/**
+	 * 取得库里存放的所有问题 author：songqi
+	 * 
+	 * @param response
+	 * @param session
+	 * @throws IOException
+	 */
+	@RequestMapping("QuestionList")
+	private void QuestionList(HttpServletResponse response, HttpSession session)
+			throws IOException {
+		response.setHeader("Cache-Control", "no-cache");
+		response.setContentType("text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		String username = (String) session.getAttribute("userName");
+		// boolean flag = false; //去数据库中判断该用户是否设置过密保问题fals表示没有验证
+		List<Pwdq> list = new ArrayList<Pwdq>();
+		Pwdq question = new Pwdq();
+		question.setPk_pwdq("1");
+		question.setPwdq("你最爱的人是谁？");
+		list.add(question);
+		Pwdq question2 = new Pwdq();
+		question2.setPk_pwdq("2");
+		question2.setPwdq("你的名字是什么？");
+		list.add(question2);
+		Gson gson = new Gson();
+		String str = gson.toJson(list);
+		out.print(str);
+	}
+
+	/**
+	 * 判断用户是否设置过密保问题。 author：songqi
+	 * 
+	 * @param response
+	 * @param session
+	 * @param opwd
+	 * @throws IOException
+	 */
+	@RequestMapping("judgeUserQuestion")
+	private void judgeUserQuestion(HttpServletResponse response,
+			HttpSession session) throws IOException {
+		response.setHeader("Cache-Control", "no-cache");
+		response.setContentType("text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		String username = (String) session.getAttribute("userName");
+		// boolean flag = false; //去数据库中判断该用户是否设置过密保问题fals表示没有验证
+		List<Pwdq> list = new ArrayList<Pwdq>();
+		Pwdq question = new Pwdq();
+		question.setPk_pwdq("1");
+		question.setPwdq("你最爱的人是谁？");
+		list.add(question);
+		Pwdq question2 = new Pwdq();
+		question2.setPk_pwdq("2");
+		question2.setPwdq("你的名字是什么？");
+		list.add(question2);
+		Gson gson = new Gson();
+		boolean flag = false;
+		flag = true;
+		if (!flag) {
+			// String str = // 获取数据库中存放的密码问题
+			String str = gson.toJson(list);
+			// System.out.println(str);
+			out.print(str);
+		} else {
+			// String str = "" // 去数据库中交互得到用户设置的内容
+			String str = gson.toJson(question2);
+			System.out.println(str);
+			out.print(str);
+		}
+	}
+
+	/**
+	 * 验证用户输入的老密码是否正确，如果正确则把新的密保问题及答案存储到数据库中，如果不正确则返回false给jsp。 author：songqi
+	 * 
+	 * @param response
+	 * @param session
+	 * @param opwd
+	 * @throws IOException
+	 */
+	@RequestMapping("/validatePwdQuestion")
+	private void validatePwdQuestion(HttpServletResponse response,
+			HttpSession session, String opwdq, String opwda, String npwdq,
+			String npwda) throws IOException {
+		PrintWriter out = response.getWriter();
+		String username = (String) session.getAttribute("userName");
+		// boolean flag = false; //去数据库中判断旧密码问题和答案是否正确
+		boolean flag = false;
+		flag = true;
+		if (!flag) {
+			out.print(false);  // 如果不正确返回false
+		} else {
+			// String str = "" // 如果旧密码问题和答案都一致，则将其修改的新密保及问题存储到数据库中
+			boolean blean = false;
+			blean = true;
+			if(blean)
+				out.print("success");
+			else
+				out.print("fail");
+		}
+	}
+
+	/**
+	 * 添加密保
+	 * author：songqi
+	 * @param response
+	 * @param session
+	 * @param nopwdQ
+	 * @param nopwdA
+	 * @throws IOException
+	 */
+	@RequestMapping("/addSecurity")
+	private void addSecurity(HttpServletResponse response,
+			HttpSession session, String nopwdQ, String nopwdA) throws IOException {
+		PrintWriter out = response.getWriter();
+		String username = (String) session.getAttribute("userName");
+		// boolean flag = false; //
+		// 需要得到主键的方法
+		// 把内容存储到数据库中方法
+		
+		boolean flag = true;
+//		flag = false;
+		out.print(flag);
 	}
 
 	/**
