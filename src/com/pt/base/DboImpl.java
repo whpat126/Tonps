10377ec4f991c5d03bd84c0c25746e2360de51f2
songqi <1668727676@qq.com>
2015-08-10 11:27:57 +0800

    dbimpl

--- src/com/pt/base/DboImpl.java
+++ src/com/pt/base/DboImpl.java
@@ -38,4 +38,8 @@ public class DboImpl implements BaseDbo {
 		return UUID.randomUUID().toString().replace("-", "");
 	}
 
+	public static void main(String[] args) {
+		System.out.println(System.currentTimeMillis());
+		
+	}
 }
